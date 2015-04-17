e91c1b174914081df80f21ca4f952abbe9cf2a39
Muhajudin <muhajudin@gmail.com>
2015-04-17 07:21:52 +0700

    revisi

--- Tugas 4/testmobil.java
+++ Tugas 4/testmobil.java
@@ -7,10 +7,10 @@ class  testmobil {
 		mobil bmw = new mobil(110,"bmw","honda","inggris");
 		mobil toyota = new mobil (90,"toyota","suzuki","perancis","biru");
 
-	System.out.println("Kecepatan Mobil = "+ferarri.getKecepatan()+"Merk Mobil = "+ferarri.getmerk()+"Mesin Mobil = "+ferarri.getMesin()+"Posisi Mobil = "+ferarri.getPosisi()+"Warna Mobil = "+ferarri.getwarna()+"Plat Nomor Mobil = "+ferrari.getPlat());
-	System.out.println("Kecepatan Mobil = "+audi.getKecepatan()+"Merk Mobil = "+audi.getmerk()+"Mesin Mobil = "+audi.getMesin()+"Posisi Mobil = "+audi.getPosisi()+"Warna Mobil = "+audi.getwarna()+"Plat Nomor Mobil = "+audi.getPlat());
-	System.out.println("Kecepatan Mobil = "+porsche.getKecepatan()+"Merk Mobil = "+porsche.getmerk()+"Mesin Mobil = "+porsche.getMesin()+"Posisi Mobil = "+porsche.getPosisi()+"Warna Mobil = "+porsche.getwarna()+"Plat Nomor Mobil = "+porsche.getPlat());
-	System.out.println("Kecepatan Mobil = "+bmw.getKecepatan()+"Merk Mobil = "+bmw.getmerk()+"Mesin Mobil = "+bmw.getMesin()+"Posisi Mobil = "+bmw.getPosisi()+"Warna Mobil = "+bmw.getwarna()+"Plat Nomor Mobil = "+bmw.getPlat());
-	System.out.println("Kecepatan Mobil = "+toyota.getKecepatan()+"Merk Mobil = "+toyota.getmerk()+"Mesin Mobil = "+toyota.getMesin()+"Posisi Mobil = "+toyota.getPosisi()+"Warna Mobil = "+toyota.getwarna()+"Plat Nomor Mobil = "+toyota.getPlat());
+	System.out.println("Kecepatan Mobil = "+ferarri.getKecepatan() + "Merk Mobil = "+ferarri.getMerk()+"Mesin Mobil = "+ferarri.getMesin()+"Posisi Mobil = "+ferarri.getPosisi()+"Warna Mobil = "+ferarri.getWarna()+"Plat Nomor Mobil = "+ferarri.getPlat());
+	System.out.println("Kecepatan Mobil = "+audi.getKecepatan()+"Merk Mobil = "+audi.getMerk()+"Mesin Mobil = "+audi.getMesin()+"Posisi Mobil = "+audi.getPosisi()+"Warna Mobil = "+audi.getWarna()+"Plat Nomor Mobil = "+audi.getPlat());
+	System.out.println("Kecepatan Mobil = "+porsche.getKecepatan()+"Merk Mobil = "+porsche.getMerk()+"Mesin Mobil = "+porsche.getMesin()+"Posisi Mobil = "+porsche.getPosisi()+"Warna Mobil = "+porsche.getWarna()+"Plat Nomor Mobil = "+porsche.getPlat());
+	System.out.println("Kecepatan Mobil = "+bmw.getKecepatan()+"Merk Mobil = "+bmw.getMerk()+"Mesin Mobil = "+bmw.getMesin()+"Posisi Mobil = "+bmw.getPosisi()+"Warna Mobil = "+bmw.getWarna()+"Plat Nomor Mobil = "+bmw.getPlat());
+	System.out.println("Kecepatan Mobil = "+toyota.getKecepatan()+"Merk Mobil = "+toyota.getMerk()+"Mesin Mobil = "+toyota.getMesin()+"Posisi Mobil = "+toyota.getPosisi()+"Warna Mobil = "+toyota.getWarna()+"Plat Nomor Mobil = "+toyota.getPlat());
 	}
 }
